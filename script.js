f133fe2849fd787fd7a72ea806bad69e18e2e557
Ben Iverson <2513iverson989@gmail.com>
2022-09-05 13:37:05 -0500

    recursion seems hard to understand, but the code came easily this time

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function isEven(num) {
+  //test for negative num, convert if needed
+  if (num < 0) {
+    num = Math.abs(num);
+  }
   //outputs a boolean value
+  if (num === 0) {
+    return true;
+  } else if (num === 1) {
+    return false;
+  } else {
+    return isEven(num - 2);
+  }
 }
